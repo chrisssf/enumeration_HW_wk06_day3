cb4765c14cf1076f6f8a4940a21285b024b6c0b7
Chris Fraser <chrisssfraser@gmail.com>
2020-01-09 00:22:22 +0000

    finished isograms

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,23 @@
 const IsogramFinder = function (word) {
-
+  this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+  let isIsogram = true;
 
-}
+  this.word.split('').forEach((wordLetter) => {
+
+    const countingLetters = this.word.split('').filter((wordLetterCheck) => {
+      return wordLetter.toLowerCase() === wordLetterCheck.toLowerCase()
+    });
+    const length = countingLetters.length
+    if (length > 1) {
+      isIsogram = false
+    }
+  });
+  return isIsogram;
+};
+
+// subde r matoglyphic
 
 module.exports = IsogramFinder;
